44d06122a304952f3a217345953c670cf3e570fa
dfuenning <dfuenning@gmail.com>
2020-04-02 20:21:56 -0400

    updating the ajax map call

--- script.js
+++ script.js
@@ -2,106 +2,113 @@ $(document).ready(function () {
 
     //Onclick that takes the city name input and uses it in citySearch function
     $("#select-city").on("click", function (event) {
-
+        $("#outputInfo").empty();
         event.preventDefault();
 
         var city = $("#city-input").val().trim();
         console.log(city)
         citySearch(city);
-    });
 
-    $(".btn-dark").on("click", function (event) {
 
-        event.preventDefault();
+        $(".btn-dark").on("click", function (event) {
 
-        console.log("OK");
-    })
+            event.preventDefault();
 
-    //Uses city from input to call API and returns results to screen
-    function citySearch(city) {
-        var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city;
+            console.log("OK");
+        })
 
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
+        //Uses city from input to call API and returns results to screen
+        function citySearch(city) {
+            var queryURL = "https://api.openbrewerydb.org/breweries?by_city=" + city;
 
-            //For loop to run through all responses
-            for (i = 0; i < response.length; i++) {
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            }).then(function (response) {
 
-                //Creates a cardbody and div to hold brewery
-                var cardDiv = $("<div class= 'card' style='width: 18rem;'>");
-                var cardBody = $("<div class= 'card-body border-dark shadow p-1 mb-2'>");
-                cardDiv.append(cardBody);
+                //For loop to run through all responses
+                for (i = 0; i < response.length; i++) {
 
-                //Populates brewery name and appends to breweryDiv
-                var breweryName = response[i].name;
+                    //Creates a cardbody and div to hold brewery
+                    var cardDiv = $("<div class= 'card' style='width: 18rem;'>");
+                    var cardBody = $("<div class= 'card-body border-dark shadow p-1 mb-2'>");
+                    cardDiv.append(cardBody);
 
-                var pOne = $("<p class= 'card-title'>").text(breweryName);
+                    //Populates brewery name and appends to breweryDiv
+                    var breweryName = response[i].name;
 
-                cardBody.append(pOne);
+                    var pOne = $("<p class= 'card-title'>").text(breweryName);
 
-                //Populates brewery address and appends to breweryDiv
-                var breweryStreet = response[i].street;
-                var breweryCity = response[i].city;
-                var breweryState = response[i].state;
-                var breweryZip = response[i].postal_code;
+                    cardBody.append(pOne);
 
-                var pTwo = $("<p class= 'card-text'>").text("Address: " + breweryStreet + ", " + breweryCity + ", " + breweryState + " " + breweryZip);
+                    //Populates brewery address and appends to breweryDiv
+                    var breweryStreet = response[i].street;
+                    var breweryCity = response[i].city;
+                    var breweryState = response[i].state;
+                    var breweryZip = response[i].postal_code;
 
-                cardBody.append(pTwo);
+                    var pTwo = $("<p class= 'card-text'>").text("Address: " + breweryStreet + ", " + breweryCity + ", " + breweryState + " " + breweryZip);
 
-                //Populates brewery phone and appends to breweryDiv
-                var breweryPhoneResponse = response[i].phone;
-                var breweryPhone1 = breweryPhoneResponse.slice(0, 3);
-                var breweryPhone2 = breweryPhoneResponse.slice(3, 6);
-                var breweryPhone3 = breweryPhoneResponse.slice(6, 10);
+                    cardBody.append(pTwo);
 
-                var pThree = $("<p class= 'card-text'>").text("Phone Number: " + breweryPhone1 + "-" + breweryPhone2 + "-" + breweryPhone3);
+                    //Populates brewery phone and appends to breweryDiv
+                    var breweryPhoneResponse = response[i].phone;
+                    var breweryPhone1 = breweryPhoneResponse.slice(0, 3);
+                    var breweryPhone2 = breweryPhoneResponse.slice(3, 6);
+                    var breweryPhone3 = breweryPhoneResponse.slice(6, 10);
 
-                cardBody.append(pThree);
+                    var pThree = $("<p class= 'card-text'>").text("Phone Number: " + breweryPhone1 + "-" + breweryPhone2 + "-" + breweryPhone3);
 
-                //Populates brewery website and appends to breweryDiv
-                var breweryWeb = response[i].website_url;
+                    cardBody.append(pThree);
 
-                var pFour = $("<p class= 'card-text'>").html("<a href='" + breweryWeb + "'>" + breweryWeb + "</a>");
+                    //Populates brewery website and appends to breweryDiv
+                    var breweryWeb = response[i].website_url;
 
-                cardBody.append(pFour);
+                    var pFour = $("<p class= 'card-text'>").html("<a href='" + breweryWeb + "'>" + breweryWeb + "</a>");
 
-                //Populates and apends map
-                var mapDiv = $("<div class= 'mapOutput'>");
+                    cardBody.append(pFour);
 
-                cardBody.append(mapDiv);
+                    //Populates and apends map
+                    var mapDiv = $("<div class= 'mapOutput'>");
 
-                var brewLat = response[i].latitude;
-                var brewLong = response[i].longitude;
+                    cardBody.append(mapDiv);
 
-                if (brewLat !== null || 0) {
+                    var brewLat = response[i].latitude;
+                    var brewLong = response[i].longitude;
 
-                    var platform = new H.service.Platform({
-                        'apikey': 'fBvknYTFknJqdfBiyf0aVH5JZjD2I_nlAFsE-GJWSX0'
-                    });
+                    if (brewLat) {
 
-                    // Obtain the default map types from the platform object:
-                    var defaultLayers = platform.createDefaultLayers();
-
-                    // Instantiate (and display) a map object:
-                    var map = new H.Map(
-                        document.getElementById('outputInfo'),
-                        defaultLayers.vector.normal.map,
-                        {
-                            zoom: 18,
-                            center: { lat: brewLat, lng: brewLong }
+                        var platform = new H.service.Platform({
+                            'apikey': 'fBvknYTFknJqdfBiyf0aVH5JZjD2I_nlAFsE-GJWSX0'
                         });
-                        var breweryMarker = new H.map.Marker({ lat: brewLat, lng: brewLong });
-                    map.addObject(breweryMarker);
 
-                    mapDiv.append(map);
+                        // Obtain the default map types from the platform object:
+                        var defaultLayers = platform.createDefaultLayers();
+
+                        // Instantiate (and display) a map object:
+                        var map = new H.Map(
+                            document.getElementById('outputInfo'),
+                            defaultLayers.vector.normal.map, {
+                                zoom: 18,
+                                center: {
+                                    lat: brewLat,
+                                    lng: brewLong
+                                }
+                            });
+                        var breweryMarker = new H.map.Marker({
+                            lat: brewLat,
+                            lng: brewLong
+                        });
+                        map.addObject(breweryMarker);
+
+                        mapDiv.append(map);
+                        console.log(map);
+                    }
+                    $("#outputInfo").append(cardBody);
                 }
-                $("#outputInfo").append(cardBody);
-            }
-        });
-    }
+            });
+        }
+    });
 
     function gameSearch() {
         var today = moment().format("YYYY-MMM-DD");
@@ -130,8 +137,3 @@ $(document).ready(function () {
     };
     gameSearch();
 });
-
-
-
-
-
